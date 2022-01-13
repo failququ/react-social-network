1319a8292a45fd2e53b09943d9bf1ea2f31d70fd
failququ <example@gmail.com>
2022-01-13 11:55:12 +0300

    initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                Main content
+            </div>
+            <div>
+                <img src="https://www.admethics.com/wp-content/uploads/2020/04/mindlake.jpg" alt="" />
+            </div>
+            <div>
+                ava + descr
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
